b4b47af1f16fb9e396bdb868e447a6f46bd8d9f8
DjSni <31524614+DjSni@users.noreply.github.com>
2022-10-12 02:35:37 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# Options for starting
+: ${USER:="root"}
+: ${GROUP:="root"}
+
+: ${CONFIG_DIR:="config"}
+
+if [ ! -d "/app/${CONFIG_DIR}" ]
+then
+  echo "Creating mount dir (/app/${CONFIG_DIR})"
+  mkdir -p /app/${CONFIG_DIR}
+  echo "Setting owner rights (${USER}:${GROUP} to /app/${CONFIG_DIR})"
+  chown -R ${USER}:${GROUP} /app/
+else
+  echo "Setting owner rights (${USER}:${GROUP} to /app/${CONFIG_DIR})"
+  chown -R ${USER}:${GROUP} /app/
+fi
+
+if [ ! -f "/app/${CONFIG_DIR}/config.default" || ! -f "/app/${CONFIG_DIR}/config.json" ]
+then
+  echo "Coping the default config, please edit it an rename it to 'config.json'."
+  cp /app/default/config.default /app/${CONFIG_DIR}/
+  exit 1
+fi
+
+echo "Starting pCloud Daemon"
+exec /app/noalbs
